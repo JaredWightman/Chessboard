b48cc565afe8c90c3d251af86f0e86500d224276
e1eish <104370928+e1eish@users.noreply.github.com>
2024-07-12 13:56:36 -0600

    King no longer gives null value errors.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -439,46 +439,55 @@ public class Board
         int pieceColor = squares[pieceFile, pieceRank].GetColor();
         int newRank;
         int newFile;
+        int moveIndex = 0;
 
 		// Forward
 		newRank = pieceRank + 1;
         newFile = pieceFile;
-        moves[0] = GiveValidCoord(newFile, newRank, pieceColor);
+        moves[moveIndex] = GiveValidCoord(newFile, newRank, pieceColor);
+        moveIndex++;
         
         // Backward
         newRank = pieceRank - 1;
         newFile = pieceFile;
-        moves[1] = GiveValidCoord(newFile, newRank, pieceColor);
+        moves[moveIndex] = GiveValidCoord(newFile, newRank, pieceColor);
+        moveIndex++;
         
         // Right
         newFile = pieceFile + 1;
         newRank = pieceRank;
-        moves[2] = GiveValidCoord(newFile, newRank, pieceColor);
+        moves[moveIndex] = GiveValidCoord(newFile, newRank, pieceColor);
+        moveIndex++;
         
         // Left
         newFile = pieceFile - 1;
         newRank = pieceRank;
-        moves[3] = GiveValidCoord(newFile, newRank, pieceColor);
+        moves[moveIndex] = GiveValidCoord(newFile, newRank, pieceColor);
+        moveIndex++;
         
         // Upper Right
         newFile = pieceFile + 1;
         newRank = pieceRank + 1;
-        moves[4] = GiveValidCoord(newFile, newRank, pieceColor);
+        moves[moveIndex] = GiveValidCoord(newFile, newRank, pieceColor);
+        moveIndex++;
         
         // Upper Left
         newFile = pieceFile - 1;
         newRank = pieceRank + 1;
-        moves[5] = GiveValidCoord(newFile, newRank, pieceColor);
+        moves[moveIndex] = GiveValidCoord(newFile, newRank, pieceColor);
+        moveIndex++;
         
         // Lower Right
         newFile = pieceFile + 1;
         newRank = pieceRank - 1;
-        moves[6] = GiveValidCoord(newFile, newRank, pieceColor);
+        moves[moveIndex] = GiveValidCoord(newFile, newRank, pieceColor);
+        moveIndex++;
         
         // Lower Left
         newFile = pieceFile - 1;
         newRank = pieceRank - 1;
-        moves[7] = GiveValidCoord(newFile, newRank, pieceColor);
+        moves[moveIndex] = GiveValidCoord(newFile, newRank, pieceColor);
+        moveIndex++;
 
 		// Castle Code (may require move index)
         if (pieceFile == 4) {
@@ -486,18 +495,27 @@ public class Board
             newFile = pieceFile + 2;
             newRank = pieceRank;
             if (squares[newFile, newRank].GetPiece() == Piece.None && squares[pieceFile + 1, pieceRank].GetPiece() == Piece.None && squares[pieceFile + 3, pieceRank].GetPiece() == Piece.Rook && squares[pieceFile + 3, pieceRank].GetColor() == pieceColor) {
-                moves[8] = GiveValidCoord(newFile, newRank, pieceColor);
+                moves[moveIndex] = GiveValidCoord(newFile, newRank, pieceColor);
+                moveIndex++;
             }
 
             // Castle queen side
             newFile = pieceFile - 2;
             newRank = pieceRank;
             if (squares[newFile, newRank].GetPiece() == Piece.None && squares[pieceFile - 1, pieceRank].GetPiece() == Piece.None && squares[pieceFile - 3, pieceRank].GetPiece() == Piece.None && squares[pieceFile - 4, pieceRank].GetPiece() == Piece.Rook && squares[pieceFile - 4, pieceRank].GetColor() == pieceColor) {
-                moves[9] = GiveValidCoord(newFile, newRank, pieceColor);
+                moves[moveIndex] = GiveValidCoord(newFile, newRank, pieceColor);
+                moveIndex++;
             }
         }
 
-		return moves;
+		Coord[] shmooves = new Coord[moveIndex];
+		moveIndex = 0;
+		foreach (Coord i in shmooves)
+		{
+			shmooves[moveIndex] = moves[moveIndex];
+			moveIndex++;
+		}
+		return shmooves;
 	}
 
     public bool IsMoveLegal(Coord startCoord, Coord endCoord)
